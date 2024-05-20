9fc15ab013658c0ee43257f3f354eeb56ff2da55
papaDhk <papa@datahawk.co>
2024-05-20 12:25:08 +0000

    test(movies-service): add integration tests

--- IntegrationTests/IntegrationTests/MoviesServiceTests.cs
+++ IntegrationTests/IntegrationTests/MoviesServiceTests.cs
@@ -1,10 +1,10 @@
+using ApiApplication.Services;
 using ApiApplication.Services.Movies;
 using FluentAssertions;
 using Microsoft.Extensions.DependencyInjection;
-using Tests.IntegrationTests;
 using Xunit;
 
-namespace Tests;
+namespace IntegrationTests;
 
 public class MoviesServiceTests : IClassFixture<TestFixture>
 {
@@ -15,7 +15,7 @@ public class MoviesServiceTests : IClassFixture<TestFixture>
     }
     
     [Fact]
-    public async Task GetAllMoviesTest()
+    public async Task GetAllMoviesAsync_ShouldReturnMovies()
     {
         await Task.Delay(10000);
 
@@ -24,29 +24,33 @@ public class MoviesServiceTests : IClassFixture<TestFixture>
         var movies = await movieService.GetAllMoviesAsync(CancellationToken.None);
 
         movies.Should().NotBeNull();
+        movies.Should().NotBeEmpty();
     }
     
     [Fact]
-    public async Task GetMovieByIdTest()
+    public async Task GetMovieByIdAsync_ShouldReturnMovie_WhenItExistInExternalMovieService()
     {
         await Task.Delay(10000);
 
         var movieService = _testFixture.ServiceProvider.GetRequiredService<IMoviesService>();
-        
-        var movie = await movieService.GetMovieByIdAsync("tt0111161",CancellationToken.None);
+
+        var imdbId = "tt0111161";
+        var movie = await movieService.GetMovieByIdAsync(imdbId,CancellationToken.None);
 
         movie.Should().NotBeNull();
+        movie.ImdbId.Should().Be(imdbId);
+        movie.Id.Should().BeGreaterThan(0);
     }
     
     [Fact]
-    public async Task GetMovieByIdNotFoundTest()
+    public async Task GetMovieByIdAsync_ShouldThrowNotFoundException_WhenTheMovieDoesNotExistInExternalService()
     {
         await Task.Delay(10000);
 
         var movieService = _testFixture.ServiceProvider.GetRequiredService<IMoviesService>();
         
-        var movie = await movieService.GetMovieByIdAsync("notExistingMovieId", CancellationToken.None);
+        await Assert.ThrowsAsync<NotFoundException>(async () =>
+            await movieService.GetMovieByIdAsync("notExistingMovieId", CancellationToken.None));
 
-        movie.Should().BeNull();
     }
 }
